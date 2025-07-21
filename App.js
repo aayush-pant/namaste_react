47a16aa55e9cd3b8e0f38977c2df2ce855f61670
Aayush Pant <aayush.pant@haptik.ai>
2025-07-22 01:13:38 +0530

    Modifying root in which react code was rendered

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Nagma:"),
-    React.createElement("h2", {}, "Bhadwa saala randibaaz"),
+    React.createElement("h3", {}, "Nagma:"),
+    React.createElement("h3", {}, "Bhadwa saala randibaaz"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Sardar:"),
-    React.createElement("h2", {}, "Ee boli humko nagma hai"),
+    React.createElement("h3", {}, "Sardar:"),
+    React.createElement("h3", {}, "Ee boli humko nagma hai"),
   ]),
 ]);
 
 console.log(parent, typeof parent); // React.createElement returns an object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root, typeof root);
-root.render(parent); // This is where the object created above is converted to HTML and placed in the DOM.
+const reactRoot = ReactDOM.createRoot(document.getElementById("reactRoot"));
+console.log(reactRoot, typeof reactRoot);
+reactRoot.render(parent); // This is where the object created above is converted to HTML and placed in the DOM.
