0f8333298130722d5799b92d7f6ada882474e589
Aayush Pant <aayush.pant@haptik.ai>
2025-10-26 16:39:18 +0530

    Installed parcel

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h3", {}, "Nagma:"),
-    React.createElement("h3", {}, "Bhadwa saala randibaaz"),
+  React.createElement("div", { id: "child1", key: "1" }, [
+    React.createElement("h3", { key: "1" }, "Nagma:"),
+    React.createElement("h3", { key: "2" }, "Bhadwa saala randibaaz"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h3", {}, "Sardar:"),
-    React.createElement("h3", {}, "Ee boli humko nagma hai"),
+  React.createElement("div", { id: "child2", key: "2" }, [
+    React.createElement("h3", { key: "1" }, "Sardar:"),
+    React.createElement("h3", { key: "2" }, "Ee boli humko nagma hai"),
   ]),
 ]);
 
