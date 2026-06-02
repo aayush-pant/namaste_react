ec4c6fd154a0e4c8b987034da4d1b3246300e2c5
Aayush Pant <aayush.pant@haptik.ai>
2026-06-02 20:32:31 +0530

    Made Changes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React.createElement => ReactElement (Object) => HTMLElement (render)
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement(
     "div",
@@ -16,7 +17,7 @@ const parent = React.createElement("div", { id: "parent" }, [
           key: "1",
           style: { display: "flex", alignItems: "center", margin: "4px" },
         },
-        "Nagma:"
+        "Nagma:",
       ),
       React.createElement(
         "h3",
@@ -24,9 +25,9 @@ const parent = React.createElement("div", { id: "parent" }, [
           key: "2",
           style: { display: "flex", alignItems: "center", margin: "4px" },
         },
-        "Bhadwa saala randibaaz..🤬"
+        "Bhadwa saala randibaaz..🤬",
       ),
-    ]
+    ],
   ),
   React.createElement(
     "div",
@@ -42,7 +43,7 @@ const parent = React.createElement("div", { id: "parent" }, [
           key: "1",
           style: { display: "flex", alignItems: "center", margin: "4px" },
         },
-        "Sardar:"
+        "Sardar:",
       ),
       React.createElement(
         "h3",
@@ -50,13 +51,24 @@ const parent = React.createElement("div", { id: "parent" }, [
           key: "2",
           style: { display: "flex", alignItems: "center", margin: "4px" },
         },
-        "Ee boli humko nagma hai..🧐"
+        "Ee boli humko nagma hai..🧐",
       ),
-    ]
+    ],
   ),
 ]);
 
+// JSX => React.createElement => ReactElement (Object) => HTMLElement (render)
+const jsxHeading = <h3 className="heading">Welcome to Namaste React 🚀</h3>;
+
 console.log(parent, typeof parent); // React.createElement returns an object
+
 const reactRoot = ReactDOM.createRoot(document.getElementById("reactRoot"));
+
 console.log(reactRoot, typeof reactRoot);
-reactRoot.render(parent); // This is where the object created above is converted to HTML and placed in the DOM.
+
+reactRoot.render(
+  <>
+    {parent}
+    {jsxHeading}
+  </>,
+); // This is where the object created above is converted to HTML and placed in the DOM.
